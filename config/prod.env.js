7ff93750b61f2ab81bb9596c933069685d73694d
arifboyz16 <arifboyz16@gmail.com>
2020-03-16 22:09:18 +0700

    setup .env netlify

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,5 +4,5 @@ module.exports = {
   VERSION: '"0.0.1"',
   VUE_APP_SECURE: true,
   VUE_APP_URL: '"jabarsurvey.digitalservice.id"',
-  VUE_APP_PORT: null
+  VUE_APP_PORT: ''
 }
--- src/views/laporan/list.vue
+++ src/views/laporan/list.vue
@@ -1,14 +1,35 @@
 <template>
-  <v-card>
-    <v-row>
-      <search />
-    </v-row>
-  </v-card>
+  <div>
+    <v-card>
+      <v-row>
+        <search
+          :handle-search="handleSearch"
+          :list-query="listQuery"
+        />
+      </v-row>
+    </v-card>
+  </div>
 </template>
 
 <script>
+import Search from '@/components/Search'
 export default {
-  name: 'LaporanList'
+  name: 'LaporanList',
+  components: {
+    Search
+  },
+  data() {
+    return {
+      listQuery: {
+        search: ''
+      }
+    }
+  },
+  methods: {
+    handleSearch() {
+      console.log('ok')
+    }
+  }
 }
 </script>
 
