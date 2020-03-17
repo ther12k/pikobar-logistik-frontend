dcbc9579f0eab20ae5ee61401cb7d9df95aadeaa
arifboyz16 <arifboyz16@gmail.com>
2020-03-17 14:24:05 +0700

    webpack

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,9 +1,8 @@
 'use strict'
-
-module.exports = (env) => {
-  NODE_ENV ='"production"',
-  VUE_APP_VERSION = '"0.0.1"',
-  VUE_APP_SECURE = true,
-  VUE_APP_URL = env.VUE_APP_URL,
-  VUE_APP_PORT = env.VUE_APP_PORT
+module.exports = {
+  NODE_ENV: '"production"',
+  VUE_APP_VERSION: '"0.0.1"',
+  VUE_APP_SECURE: true,
+  VUE_APP_URL: 'api.pelaporan.pikobar.jabarprov.go.id',
+  VUE_APP_PORT: '""'
 }
