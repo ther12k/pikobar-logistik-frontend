03a401d9f7765608f1d772f1e349cfbf77cfbfbd
maruf12 <maruf.harsono@gmail.com>
2020-03-18 20:22:39 +0700

    design dashboard

---
+++ src/components/Dashboard/Data.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-card
+    max-width="100%"
+    class=""
+  >
+    <v-container>
+      <v-row>
+        <v-col cols="3" width="25%">
+          <v-card
+            color="info"
+            dark
+          >
+            <v-card-title class="headline text-center">Total Pasien</v-card-title>
+
+            <v-spacer/>
+
+            <v-card-subtitle class="text-center display-3	">{{ patien.TOTAL }}</v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn text>Lihat Detail</v-btn>
+            </v-card-actions>
+
+          </v-card>
+        </v-col>
+        <v-col cols="3" width="25%">
+          <v-card
+            color="purple"
+            dark
+          >
+            <v-card-title class="headline text-center">ODP</v-card-title>
+
+            <v-spacer/>
+
+            <v-card-subtitle class="text-center display-3">{{ patien.ODP }}</v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn text>Lihat Detail</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="3" width="25%">
+          <v-card
+            color="warning"
+            dark
+          >
+            <v-card-title class="headline text-center">PDP</v-card-title>
+
+            <v-spacer/>
+
+            <v-card-subtitle class="text-center display-3">{{ patien.PDP }}</v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn text>Lihat Detail</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="3" width="25%">
+          <v-card
+            color="pink"
+            dark
+          >
+            <v-card-title class="headline text-center">Positif</v-card-title>
+
+            <v-spacer/>
+
+            <v-card-subtitle class="text-center display-3">{{ patien.POSITIF }}</v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn text>Lihat Detail</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'CardData',
+  data: () => ({
+    patien: {
+      ODP: 0,
+      PDP: 0,
+      POSITIF: 0,
+      TOTAL: 0
+    }
+  }),
+  async mounted() {
+    const data = await this.$store.dispatch('reports/countReportCase')
+    this.patien = await data.data
+    console.log(this.patien)
+    for (var key in this.patien) {
+      this.patien.TOTAL += this.patien[key]
+    }
+  }
+}
+</script>
