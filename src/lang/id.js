1cb3cdf161dc029b09cd4bcfb3353aa7c8850a7c
arifboyz16 <arifboyz16@gmail.com>
2020-03-27 18:10:01 +0700

    change menu

--- src/lang/id.js
+++ src/lang/id.js
@@ -4,9 +4,9 @@ export default {
   route: {
     dashboard: 'Dashboard',
     laporan_title: 'Laporan Kasus',
-    rdt: 'RDT',
-    create_rdt: 'Tambah RDT',
-    list_rdt: 'Daftar RDT',
+    rdt: 'Test Covid-19',
+    create_rdt: 'Tambah Peserta',
+    list_rdt: 'List Peserta',
     rdt_detail: 'Detail Peserta',
     rdt_update: 'Update Hasil Peserta',
     medical_personnel: 'Tenaga Kesehatan',
