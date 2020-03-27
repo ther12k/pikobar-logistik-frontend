abb2f17267698836a0c2e6b316c6d5bf9e456a3b
arifboyz16 <arifboyz16@gmail.com>
2020-03-27 22:59:39 +0700

    list bugs

--- src/views/rdt/list.vue
+++ src/views/rdt/list.vue
@@ -85,10 +85,10 @@
                           <v-list-item @click="handleDetail(item._id)">
                             Lihat Detail
                           </v-list-item>
-                          <v-list-item v-if="item.final_result.length < 1" @click="handleEditRDT(item._id)">
+                          <v-list-item v-if="item.final_result && item.final_result.length > 0" @click="handleEditRDT(item._id)">
                             Update Profil Peserta
                           </v-list-item>
-                          <v-list-item v-if="item.final_result.length < 1 " @click="handleUpdateResults(item._id)">
+                          <v-list-item v-if="item.final_result && item.final_result.length > 0 " @click="handleUpdateResults(item._id)">
                             Update Hasil
                           </v-list-item>
                           <v-list-item @click="handleDeleteRDT(item._id)">
