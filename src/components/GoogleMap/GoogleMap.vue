3cded2e1478c9210a00a5217880f6521aa488737
fathur <fathur1097@gmail.com>
2020-04-27 13:18:03 +0700

    component maps

--- src/components/GoogleMap/GoogleMap.vue
+++ src/components/GoogleMap/GoogleMap.vue
@@ -13,7 +13,7 @@
     <gmap-map
       :center="center"
       :zoom="12"
-      style="width:100%  height: 400px"
+      style="width:100%;  height: 400px;"
     >
       <gmap-marker
         v-for="(m, index) in markers"
